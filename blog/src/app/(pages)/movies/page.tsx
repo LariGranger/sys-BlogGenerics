656b3dc912d2eb4a1086eda3d72904e804ec0fe3
Larissa Pinheiro <116392603+LariGranger@users.noreply.github.com>
2025-05-23 22:57:30 +0000

    new: new project

---
+++ blog/src/app/(pages)/movies/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+const Movies = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Movies;
